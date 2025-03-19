48080c7ad7e5a7345b15c8e9170ac8ace4e48a12
Kamilly Mattana <pkamillymattana@gmail.com>
2025-03-19 19:38:19 -0300

    Realizacao da atividade

---
+++ ativ01.py
@@ -0,0 +1,7 @@
+nome=input("Digite seu nome")
+cpf=str(input("Digite seu cpf"))
+tel=int(input("DIgite seu telefone"))
+dtNasc=int(input("Digite sua data de nascimento"))
+
+print(nome,cpf,tel,dtNasc)
+print("Obrigado!")
