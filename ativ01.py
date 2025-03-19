7665077b6b430d737b93f1e66c8869a96e9c38f9
Kamilly Mattana <pkamillymattana@gmail.com>
2025-03-19 19:51:00 -0300

    Alteracao do codigo

--- ativ01.py
+++ ativ01.py
@@ -3,9 +3,9 @@ cpf=str(input("Digite seu cpf"))
 tel=int(input("DIgite seu telefone"))
 dtNasc=str(input("Digite sua data de nascimento"))
 
-print("Seu nome e "nome)
-print("Seu CPF e "cpf)
-print("Seu telefone e "tel)
-print("Sua data de nascimento e "dtNasc)
+print("Seu nome e "+nome)
+print("Seu CPF e "+cpf)
+print("Seu telefone e "+tel)
+print("Sua data de nascimento e "+dtNasc)
 
 print("Obrigado!")
