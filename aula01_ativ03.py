0ce423163448598a0c6a1aaeaeb4b5c35d64d9de
Kamilly Mattana <pkamillymattana@gmail.com>
2025-03-19 22:22:34 -0300

    Update aula01_ativ03.py

--- aula01_ativ03.py
+++ aula01_ativ03.py
@@ -1,1 +1,70 @@
+# Lista de exercicios (6 exercicios no total)
 
+print("Exercicio 01")
+idade=int(input("Digite sua idade: "))
+ano=int(input("Digite o ano em que esta: "))
+
+dtNasc=ano-idade
+
+print("Você nasceu no ano: ",dtNasc)
+
+
+print("=====================================")
+
+
+print("Exercicio 02")
+print("O valor de cada carro e de R$ 100,00")
+quantiCarro=int(input("Quantos carros gostaria de alugar?"))
+
+valorCarro=100
+total=quantiCarro*valorCarro
+
+print("O valor total da compra foi: ", total)
+
+
+print("=====================================")
+
+
+print("Exercicio 03")
+celcius=float(input("Digite quantos graus (celsius) estao agora: "))
+
+fahrenheit=((celcius*9)/5)+32
+
+print("Os graus atuais transcritos em fahrenheit sao: ",fahrenheit)
+
+
+print("=====================================")
+
+
+print("Exercicio 04")
+nota1=float(input("Digite a nota 01: "))
+nota2=float(input("Digite a nota 02: "))
+nota3=float(input("Digite a nota 03: "))
+nota4=float(input("Digite a nota 04: "))
+
+media=(nota1+nota2+nota3+nota4)/4
+
+print("A media das suas notas e igual a: ",media)
+
+
+print("=====================================")
+
+
+print("Exercicio 05")
+idade=int(input("Digite sua idade: "))
+
+meses=idade*12
+
+print("Sua idade em meses e igual a: ",meses)
+
+
+print("=====================================")
+
+
+print("Exercicio 06")
+preco=float(input("Digite o preco do produto: "))
+kilo=float(input("Digite quantos kilos você gostaria de comprar: "))
+
+total=preco*kilo
+
+print("O preco final e igual a: ",total)
