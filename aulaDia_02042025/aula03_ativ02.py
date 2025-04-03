cc4e7874f11b4e780a0ee5aca15082d6e3d318d6
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-02 22:37:26 -0300

    historinha

--- aulaDia_02042025/aula03_ativ02.py
+++ aulaDia_02042025/aula03_ativ02.py
@@ -93,3 +93,31 @@ while True:
         break
 
 # EXERCICIO 02
+print("Você acorda em uma floresta escura e nao se lembra de nada.")
+print("Nao sabe nem como chegou ali.")
+print("Os feixes do brilho da lua mostram um caminho de terra.")
+print("Você se levanta e segue o caminho.")
+print("Você encontra duas opcoes para seguir.")
+print("As opcoes sao: o caminho da esquerda ou o caminho da direita")
+caminho=str(input("Digite E para ESQUERDA | D para DIREITA: "))
+
+if caminho=="E" or caminho=="e":
+    print("=================")
+    print("Você escolhe o caminho a ESQUERDA")
+    print("Seguindo por este caminho você chega a um pqueno rio.")
+    print("Do outro lado ha uma luz ao longe.")
+    print("Você observa o reflexo da lua na agua e pensa: 'Parece fundo...'")
+    print("S para SIM | N para NAO")
+    caminhoE=str(input("Deseja seguir em frente? "))
+    if caminhoE=="S" or "s":
+        print("=================")
+        print("Sua curiosidade grita alto. VOCÊ DECIDE SEGUIR")
+        print("Você toca na agua com timidez.")
+        print("'Esta um gelo' - você pensa consigo.")
+        print("Porem mesmo com receio você segue em frente.")
+        print("A agua sobe lentamente ate seus joelhos.")
+        print("Apesar do medo você persiste e chega ao outro lado.")
+        print("Olhando para o horizonte a luz foge.")
+        print("O medo e desespero lhe dominam.")
+        print("Derepente surge um pequeno ponto de luz")
+        print("Era um vagalume")
