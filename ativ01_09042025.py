da1453b708cc5b360df87977e9a3763381d7e704
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-09 19:45:36 -0300

    Create ativ01_09042025.py

---
+++ ativ01_09042025.py
@@ -0,0 +1,25 @@
+# exercicio 01
+numero=int(input("Favor digite um numero: "))
+
+01=numero*1
+02=numero*2
+03=numero*3
+04=numero*4
+05=numero*5
+06=numero*6
+07=numero*7
+08=numero*8
+09=numero*9
+010=numero*10
+
+print("\nEsta eh a tabuada do numero",numero,)
+print(numero,"* 1 = ",01)
+print(numero,"* 2 = ",02)
+print(numero,"* 3 = ",03)
+print(numero,"* 4 = ",04)
+print(numero,"* 5 = ",05)
+print(numero,"* 6 = ",06)
+print(numero,"* 7 = ",07)
+print(numero,"* 8 = ",08)
+print(numero,"* 9 = ",09)
+print(numero,"* 10 = ",10)
