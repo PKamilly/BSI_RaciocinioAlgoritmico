861ad4c9c6f0a9056eb10738e7f34f8cfca974fa
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-09 11:43:09 -0300

    Update aula03_ativ02.py

--- aulaDia_02042025/aula03_ativ02.py
+++ aulaDia_02042025/aula03_ativ02.py
@@ -94,57 +94,58 @@ while True:
 
 # EXERCICIO 02
 # Para uma melhor experiência, rode o programa :)
-nome=input("Digite seu nome: ")
+nome = input("Digite seu nome: ")
 
-print("======================")
+print("\n======================")
 
 print("Você acorda deitado no chão de uma floresta escura, não se sabe como chegou ali.")
 print("Tocando em seu rosto para tentar acordar deste sonho, você o sente gelado.")
-print("Meio zonzo e confuso você se senta, ao fazer isto você instantaneamente sente uma fadiga e dor no corpo extrema.")
+print(
+    "Meio zonzo e confuso você se senta, ao fazer isto você instantaneamente sente uma fadiga e dor no corpo extrema.")
 print("Em meio ao som ambiental você ouve seu estômago implorando por alimento. Você está faminto.")
 print("Por quanto tempo estava deitado ali? por horas? por dias? ou será por semanas?")
 print("Analizando ao redor há somente árvores altas, arbustos pequenos e ornamentais e rochas ao chão.")
 print("Os feixes do brilho da lua iluminam levemente a floresta, mostrando um caminho de terra.")
 import time
-tempo=10
+tempo = 15
 time.sleep(tempo)
 
-print("Seu estômago ronca.")
+print("\nSeu estômago ronca.")
 print("Sem muitas opções, você se levanta do chão com dificuldades e segue o caminho sem um rumo e sem esperanças.")
 print("----------------------")
 print("5 minutos depois . . .")
 print("----------------------")
 import time
-tempo=4
+tempo = 4
 time.sleep(tempo)
-    
-print("Após um tempo caminhando você se depara com uma bifurcação.")
-print("Os caminhos são idênticos.")
+
+print("\nApós um tempo caminhando você se depara com uma bifurcação.")
+print("Os caminhos são idênticos. Porém nota-se uma montanha no caminho a direita.")
 print("Seu estômago ronca.")
 print("Você precisa decidir por qual caminho seguir.")
 print("Digite E para ESQUERDA | D para DIREITA")
-caminho=str(input("R: "))
+caminho = str(input("R: "))
 
-if caminho=="E" or caminho=="e":
-    print("=================")
+if caminho == "E" or caminho == "e":
+    print("\n=================")
     print("O caminho a ESQUERDA lhe chama a atenção, você decide segui-lo.")
     print("----------------------")
     print("3 minutos depois . . .")
     print("----------------------")
     import time
-    tempo=4
+    tempo = 4
     time.sleep(tempo)
-    
-    print("Você chega a um pequeno rio.")
+
+    print("\nVocê chega a um pequeno rio.")
     print("Do outro lado há uma pequena luz ao longe.")
     print("'Aquela luz pode ser um acampamento!' - você pensa consigo coberto de esperança.")
-    print("Desviando o olhar você observa o reflexo da Lua na leve correntesa que se forma e pensa consigo: 'Parece fundo...'")
+    print("Desviando o olhar você observa o reflexo da Lua na correntesa constante que se forma e pensa consigo: 'Parece fundo...'")
     print("Seu estômago ronca.")
     print("Digite S para SIM | N para NÃO")
-    rio=str(input("Você deveria seguir em frente? "))
-    
-    if rio=="S" or rio=="s":
-        print("=================")
+    rio = str(input("Você deveria seguir em frente? "))
+
+    if rio == "S" or rio == "s":
+        print("\n=================")
         print("Sua curiosidade grita alto. VOCÊ DECIDE SEGUIR.")
         print("Focado(a) você toca seus pés na água com timidez.")
         print("'Esta um gelo' - você pensa consigo.")
@@ -152,50 +153,149 @@ if caminho=="E" or caminho=="e":
         print("Mesmo com receio você segue em frente.")
         print("A cada passo adentro, seu corpo arrepia cada vez mais até alma.")
         print("Você ouve algo baixo atrás de si mas ignora, você não se importa e segue adiante.")
-        print("A água sobe lentamente até seus joelhos.")
-        print("Apesar do medo você persiste contra a leve correntesa e chega ao outro lado.")
+        print("A água sobe lentamente até sua cintura, provando a sua teoria.")
+        print("Apesar do medo você persiste cuidadosamente contra a correntesa")
+        print("Seu pé se prende em algo abaixo da água, mas você consegue se soltar e chega ao outro lado.")
         print("Olhando para o horizonte a luz foge.")
-        print("O medo e desespero lhe dominam.")
-        print("De repente surge um pequeno ponto de luz.")
+        print("O medo e desespero lhe dominam.\n")
+        import time
+        tempo=10
+        time.sleep(tempo)
+
+        print("De repente surge um pequeno ponto de luz.\n")
+        import time
+        tempo=3
+        time.sleep(tempo)
+
         print("Era apenas um vagalume.")
-        
-    elif rio=="N" or rio=="n":
-        print("=================")
+
+    elif rio == "N" or rio == "n":
+        print("\n=================")
         print("Após analisar mais um pouco o rio, o medo lhe consome. VOCÊ DECIDE FICAR.")
         print("Alguns segundos depois você ouve um arbusto se mexer por perto.")
-        print("Virando para trás você nota uma sombra em meio aos arbustos.")
+        print("Virando para trás você nota um pequeno vulto em meio aos arbustos.")
         print("Digite S para SIM | N para NÃO")
-        arbusto=str(input("Checar o arbusto? "))
-        if arbusto=="s" or arbusto=="S":
-            print("=================")
+        arbusto = str(input("Checar o arbusto? "))
+        if arbusto == "s" or arbusto == "S":
+            print("\n=================")
             print("Você é medroso porém curioso. VOCÊ DECIDE CHECAR.")
             print("Receoso você continua lentamente e sente que cada passo parece uma eternidade.")
             print("De repente você sente algo no pé e logo em seguida ouve um barulho, um galho se partiu.")
             print("Você paralisa e descobre a origem do barulho, seus pés.")
             print("Você acidentalmente pisou em um galho, estava tão amedrontado que nem notou o caminho a sua volta.")
-            print("Silêncio.")
-            print("Olhando ao redor não pareceu acontecer nada.")
+            import time
+            tempo=13
+            time.sleep(tempo)
+
+            print("\nSilêncio.")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
+            print("\nOlhando ao redor não pareceu acontecer nada.")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
             print("Nenhum peixe pulou da água")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
             print("Nenhum grillo cantarolava")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
             print("Nenhuma folha caiu de sua árvore.")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
             print("Nenhuma coruja chirriou ao longe.")
+            import time
+            tempo=3
+            time.sleep(tempo)
+
             print("Somente o eterno silêncio.")
-            print("Seu estômago ronca alto.")
-            print("Neste momento você sente o enorme arrependimento engolindo seu corpo.")
-            print("Em seguida um vulto preto petróleo com o dobro do seu tamanho surge por entre os arbustos.")
+            import time
+            tempo=5
+            time.sleep(tempo)
+
+            print("\nSeu estômago ronca alto.")
+            import time
+            tempo=5
+            time.sleep(tempo)
+
+            print("\nNeste momento você sente o enorme arrependimento engolindo seu corpo.")
+            import time
+            tempo=4
+            time.sleep(tempo)
+
+            print("\nEm seguida aquele vulto preto petróleo com o dobro do seu tamanho surge por entre os arbustos.")
             print("Aqueles mesmos em que você estava prestes a checar a alguns segundos atrás.")
+            import time
+            tempo=6
+            time.sleep(tempo)
+
+            print("O vulto se espreita e para.")
+            print("O luar da Lua mostra sua verdadeira face peluda em tom cacau.")
+            import time
+            tempo = 5
+            time.sleep(tempo)
+
+            print("A fera lhe encara.")
+            import time
+            tempo = 3
+            time.sleep(tempo)
+
+            print("\n'Porra' -",nome,", 2025.")
+        else:
+            print("\nSeu corpo paralisa. VOCÊ NÃO FAZ NADA.")
+            print("De repente o vulto se espreita e para.")
             print("O luar da Lua mostra sua verdadeira face peluda em tom cacau.")
             print("A fera lhe encara.")
-            print("'Porra' - ",nome,", 2025.")
-        else:
-            print("Seu corpo paralisa. VOCÊ NÃO FAZ NADA.")
-            print("Virando lentamente você ")
-            
-elif caminho=="D" or caminho=="d":
-    print("=================")
-    print("Sua intuição te manda seguir pela direita. VOCÊ SEGUE O CAMINHO A DIREITA.")
-    print("Caminhando por alguns minutos você nota uma montanha ao longe")
+            import time
+            tempo=8
+            time.sleep(tempo)
+
+            print("\nSuando frio e com o rosto mais pálido que antes, VOCÊ DECIDE ATRAVESSAR O RIO.")
+            print("Você corre até o rio e entra.")
+            print("A água está um gelo mas não importa agora.")
+            print("A água sobe até sua cintura provando a sua teoria.")
+            import time
+            tempo=8
+            time.sleep(tempo)
+
+            print("\nEm um passo em falso você prende seu pé em algo.")
+            print("'MERDA!' - você grita e ouve um grunido vindo de trás e se vira.")
+            print("A fera peluda não está mais entre os arbustos, ela caminha para perto do rio lhe encarando.")
+            print("Você sente seus olhos arderem e começarem a molhar.")
+            print("O desespero lhe consome.")
+            import time
+            tempo=6
+            time.sleep(tempo)
+
+            print("\nPor conta da fadiga, dor e fome você cansa mais rápido mas luta pela sobrevivência.")
+            print("")
+
+elif caminho == "D" or caminho == "d":
+    print("\n=================")
+    print("'La de cima posso ter uma ampla visão da área' - você fala sozinho. VOCÊ SEGUE O CAMINHO A DIREITA.")
+    print("Caminhando por alguns minutos você chega ao pé da montanha.")
+    print("Ela é bem íngreme e rochosa.")
+    print("Seu estômago ronca.")
     print("Digite S para SIM | N para NÃO")
-    montanha=str(input("Seguir e subir a montanha? "))
-    if montanha=="S" or montanha=="s":
-        print("'La de cima posso ter uma ampla visão da área' - você pensa consigo mesmo.")
+    caminhoD = str(input("Seguir e subir a montanha? "))
+    if caminhoD == "S" or caminhoD == "s":
+        print("\nApesar do extremo cansaso. VOCÊ DECIDE SUBIR.")
+        print("----------------------")
+        print("5 minutos depois . . .")
+        print("----------------------")
+        import time
+        tempo = 3
+        time.sleep(tempo)
+                   
+        print("\nSubindo com muito cuidado você olha em volta e nota um rio.")
+        print("'Será que eu chegaria naquele rio indo pelo outro caminho?' - você pensa consigo.")
+        print("Distraído com o rio você escorrega e acaba caindo")
