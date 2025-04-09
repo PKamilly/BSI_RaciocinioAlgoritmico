02002263b4408a51cac7c87953b1a26ab3e941e9
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-09 20:39:54 -0300

    Update ativ01_09042025.py

--- ativ01_09042025.py
+++ ativ01_09042025.py
@@ -1,25 +1,13 @@
 # exercicio 01
 numero=int(input("Favor digite um numero: "))
 
-01=numero*1
-02=numero*2
-03=numero*3
-04=numero*4
-05=numero*5
-06=numero*6
-07=numero*7
-08=numero*8
-09=numero*9
-010=numero*10
+multiplicador=0
+numMultip=0
 
-print("\nEsta eh a tabuada do numero",numero,)
-print(numero,"* 1 = ",01)
-print(numero,"* 2 = ",02)
-print(numero,"* 3 = ",03)
-print(numero,"* 4 = ",04)
-print(numero,"* 5 = ",05)
-print(numero,"* 6 = ",06)
-print(numero,"* 7 = ",07)
-print(numero,"* 8 = ",08)
-print(numero,"* 9 = ",09)
-print(numero,"* 10 = ",10)
+while multiplicador<=10 and numMultip<=10:
+    total=numero*numMultip
+    print(numero,"*",numMultip,"=",total)
+    multiplicador=multiplicador+1
+    numMultip=numMultip+1
+
+#exercicio 02
