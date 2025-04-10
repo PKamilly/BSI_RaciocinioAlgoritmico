2c3e57b60230a073fbc438922ca00eb93ecfa533
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-09 21:52:35 -0300

    Update ativ01_09042025.py

--- ativ01_09042025.py
+++ ativ01_09042025.py
@@ -20,3 +20,18 @@ while nota<=7:
         print("\nNota valida!")
 
 # exercicio 03
+soma=0
+contador=0
+
+numero=float(input("Digite um numero: "))
+
+while numero!= -1:
+   soma += numero
+   #"soma += numero" e igual a "soma=soma+numero"
+   numero=float(input("Digite um numero: "))
+   contador += 1
+   
+total=soma/contador
+print("A media dos numeros e igual a",total)
+
+# exercicio 04
