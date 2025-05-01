f07c71edfb7c697ba91d13e6a2fe533f0f01a3ac
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-04-30 21:56:45 -0300

    realização dos exercícios

--- aulaDia_30042025/ativ.py
+++ aulaDia_30042025/ativ.py
@@ -7,6 +7,7 @@ print(numeros[2])
 print(numeros[3])
 print(numeros[4])
 print(numeros[5])
+print("=============")
 
 # EXERCÍCIO 2
 numeros[1]=17
@@ -14,14 +15,24 @@ numeros[2]=1
 numeros[3]=22
 numeros[4]=29
 print(numeros)
+print("=============")
 
 # EXERCÍCIO 3
-soma=numeros[5]+numeros[3]
+soma=numeros[5]+numeros[4]
 sub=numeros[3]-numeros[1]
 multi=numeros[0]*numeros[5]
 div=numeros[3]/numeros[2]
+print("=============")
 
 print("SOMA do número",numeros[5],"com o número",numeros[3],"da",soma)
 print("SUBTRAÇÃO do número",numeros[3],"com o número",numeros[1],"da",sub)
 print("MULTIPLICAÇÃO do número",numeros[0],"com o número",numeros[5],"da",multi)
 print("SOMA do número",numeros[3],"com o número",numeros[2],"da",div)
+print("=============")
+
+# EXERCÍCIO 4
+indice=0
+
+while indice<6:
+    print(numeros[indice]*2)
+    indice+=1
