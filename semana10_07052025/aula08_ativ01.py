fa60028d91cb59ae8fb9a0147203093c91cbc24a
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-05-07 19:39:32 -0300

    a

---
+++ semana10_07052025/aula08_ativ01.py
@@ -0,0 +1,20 @@
+# EXERCICIO 01
+contador=0
+
+for i in range(0,10):
+    if contador % 2 != 0:
+        print(f"{contador} é ímpar.")
+    else:
+        print(f"{contador} é par.")
+    
+    contador = contador + 1
+
+# EXERCICIO 02
+letras = ["P","r", "o", "f", "e"]
+indice = 0
+
+for i in range(0,5):
+    print(letras[indice])
+    indice = indice + 1
+
+# EXERCICIO 03
