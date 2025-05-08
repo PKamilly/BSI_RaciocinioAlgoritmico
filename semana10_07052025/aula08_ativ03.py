19cf8684b652ee497e2ec01aa0f2544a5f50bce2
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-05-08 09:28:08 -0300

    a

--- semana10_07052025/aula08_ativ03.py
+++ semana10_07052025/aula08_ativ03.py
@@ -1,10 +1,26 @@
 # EXERCÍCIO 01
-
+for i in range(1,50):
+    print(i)
 
 # EXERCÍCIO 02
+par=0
 
+for i in range(1,100):
+    print(i)
+    if i % 2 == 0:
+        par+=1
+
+print(f"Foram encontrados {par} números pares.")
 
 # EXERCÍCIO 03
+valorInt=int(input("Digite um valor inteiro entre 0 e 10: "))
+
+if 0<=valorInt<=10:
+    for i in range(0,11):
+        multi=valorInt*i
+        print(multi)
+else:
+    print("Digite um número inteiro válido!")
 
 
 # EXERCÍCIO 04
