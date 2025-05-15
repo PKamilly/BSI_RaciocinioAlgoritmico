75a002f937c38e7eb8ad55febfd589f4b0f45af4
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-05-14 21:47:13 -0300

    a

---
+++ semana11_14052025/aula09_ativ02.py
@@ -0,0 +1,26 @@
+# EXERCÍCIO 01
+import random
+lista=[]
+
+lista=random.randint(0,100)
+
+print(f"Valor da lista: {lista}")
+
+# EXERCÍCIO 02
+lista=[]
+
+for i in range(0,3):
+    num=int(input("Digite um número: "))
+    lista+=[num]
+
+print(lista)
+
+# EXERCICIO 03
+frase=str(input("Digite uma frase: "))
+
+for palavra in frase:
+    palavra=frase.split()
+    
+print(palavra)
+
+# EXERCICIO 04
