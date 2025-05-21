1f61798a1ea810fd43303309ed4f625b9c542e66
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-05-21 19:49:00 -0300

    a

--- semana12_21052025/aula10_ativ01.py
+++ semana12_21052025/aula10_ativ01.py
@@ -11,4 +11,29 @@ print("="*25)
 for posicao in matriz:
     print(posicao)
 
+print("="*25)
+
+for linha in matriz:
+    for coluna in matriz:
+        print(matriz[linha][coluna])
+
 # EXERCÍCIO 02
+matriz=[
+    [20,2,3],
+    [4,5,15],
+    [7,19,9],
+]
+
+print(matriz)
+print("="*25)
+
+for posicao in matriz:
+    print(posicao)
+
+print("="*25)
+
+for linha in range(3):
+    for coluna in range(3):
+        print(matriz[linha][coluna])
+
+# EXERCÍCIO 03
