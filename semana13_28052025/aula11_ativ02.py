3c4d5bad163c3279985f003656200b02fddae375
Kamilly Victória Mattana Prestes <pkamillymattana@gmail.com>
2025-06-04 19:20:51 -0300

    Update aula11_ativ02.py

--- semana13_28052025/aula11_ativ02.py
+++ semana13_28052025/aula11_ativ02.py
@@ -82,4 +82,57 @@ while operacao != 5:
     if operacao == 5:
         agradecimentos()
         break
-    
+
+# Alternativa melhor (corrigido)
+#def menu():
+#    print("=" * 25)
+#    print("CALCULADORA")
+#    print("=" * 25)
+#    print("1. SOMA")
+#    print("2. SUBTRAÇÃO")
+#    print("3. MULTIPLICAÇÃO")
+#    print("4. DIVISÃO")
+#    print("5. SAIR")
+#
+#def agradecimentos():
+#    print("Obrigada por usar a calculadora!")
+#
+#def soma(x, y):
+#    return x + y
+#
+#def sub(x, y):
+#    return x - y
+#
+#def mult(x, y):
+#    return x * y
+#
+#def div(x, y):
+#    return x / y
+#
+#operacao = 1
+#num1 = 0
+#num2 = 0
+#
+#while 1 <= operacao <= 5:
+#    menu()
+#    print("=" * 25)
+#    operacao = int(input("Escolha sua operação: "))
+#    print("=" * 25)
+#
+#    if operacao != 5:
+#        num1 = int(input("Digite o primeiro valor: "))
+#        num2 = int(input("Digite o segundo valor: "))
+#        print("=" * 25)
+#
+#
+#    if operacao == 1:
+#        print(f"Soma: {soma(num1, num2)}")
+#    elif operacao == 2:
+#        print(f"Subtração: {sub(num1, num2)}")
+#    elif operacao == 3:
+#        print(f"Multiplicação: {mult(num1, num2)}")
+#    elif operacao == 4:
+#        print(f"Divisão: {(num1, num2)}")
+#    elif operacao == 5:
+#        agradecimentos()
+#        break
